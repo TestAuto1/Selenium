bc6b0ec67edfb9822bd66a608449a42360670a80
GamilM <GamilM@E0366482.internal.vodafone.com>
2018-01-31 15:18:50 +0200

    Test commit

---
+++ SeleniumWithMavenandJenkins/src/test/java/MavenTest/TC001.java
@@ -0,0 +1,16 @@
+package MavenTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC001 {
+	@Test				
+	public void Dummy_test_to_pass() 
+	{									 
+	Assert.assertEquals(1,1); 		
+	}	
+	@Test				
+	 public void Dummy_test_to_fail()
+	 {								 
+	Assert.assertEquals(1,2); 		
+	}
+}
